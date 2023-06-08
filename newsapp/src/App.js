d64d48516e39029a40e6cc5a563044cc47490094
Suyog Acharya <asuyog042@gmail.com>
2023-06-08 09:23:44 +0545

    card added

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,18 +1,28 @@
 //import logo from './logo.svg';
 import "./App.css";
 import React, { Component } from "react";
-import Navbar from "./components/Navbar";
-import News from "./components/News";
-//import Newsitem from "./components/Newsitem";
+//import Navbar from "./components/Navbar";
+//import News from "./components/News";
+import Newsitem from "./components/NewsItem";
 //class based component
 export default class App extends Component {
   render() {
     //lifecycle method
 
     return (
-      <div>
-        <Navbar />
-        <News />
+      <div className="container my-3">
+        <h2>News app- Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <Newsitem title="myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <Newsitem title="myTitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <Newsitem title="myTitle" description="mydesc" />
+          </div>
+        </div>
       </div>
     );
   }
