52fbb898dd012ec8201b23dc336c1312236b62fc
Suyog Acharya <asuyog042@gmail.com>
2023-06-08 19:25:20 +0545

    article state added

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,28 +1,40 @@
 //import logo from './logo.svg';
 import "./App.css";
 import React, { Component } from "react";
-//import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 //import News from "./components/News";
 import Newsitem from "./components/NewsItem";
+import News from "./components/News";
 //class based component
 export default class App extends Component {
   render() {
     //lifecycle method
 
     return (
-      <div className="container my-3">
-        <h2>News app- Top Headlines</h2>
-        <div className="row">
-          <div className="col-md-4">
-            <Newsitem title="myTitle" description="mydesc" />
-          </div>
-          <div className="col-md-4">
-            <Newsitem title="myTitle" description="mydesc" />
-          </div>
-          <div className="col-md-4">
-            <Newsitem title="myTitle" description="mydesc" />
-          </div>
-        </div>
+      // <div className="container my-3">
+      //   <Navbar />
+      //   <h2>News app- Top Headlines</h2>
+      //   <div className="row">
+      //     <div className="col-md-4">
+      //       <Newsitem
+      //         title="myTitle"
+      //         description="mydesc"
+      //         url="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+      //         newsurl="todo"
+      //       />
+      //     </div>
+      //     <div className="col-md-4">
+      //       <Newsitem title="myTitle" description="mydesc" />
+      //     </div>
+      //     <div className="col-md-4">
+      //       <Newsitem title="myTitle" description="mydesc" />
+      //     </div>
+      //   </div>
+      // </div>
+
+      <div>
+        <Navbar />
+        <News />
       </div>
     );
   }
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -2,21 +2,19 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, url, newsurl } = this.props;
     return (
       <div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}.</p>
-            <a href="/" className="btn btn-primary">
-              Click to preview
-            </a>
+        <div className="my-3">
+          <div className="card" style={{ width: "18rem" }}>
+            <img src={url} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}.</p>
+              <a href="/" className="btn btn-primary btn-sm">
+                Read more
+              </a>
+            </div>
           </div>
         </div>
       </div>
