54e67af1f509b1f9c2941574102ba69deeeeb62d
Suyog Acharya <asuyog042@gmail.com>
2023-06-10 16:42:04 +0545

    fetched newsitem using api key

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -4,77 +4,59 @@ import React, { Component } from "react";
 import Newsitem from "./NewsItem";
 import Navbar from "./Navbar";
 export class News extends Component {
-  // articles = [
-  //   {
-  //     source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-  //     author: null,
-  //     title:
-  //       "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-  //     description:
-  //       "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-  //     url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-  //     urlToImage:
-  //       "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-  //     publishedAt: "2020-04-27T11:41:47Z",
-  //     content:
-  //       "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-  //   },
-  //   {
-  //     source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-  //     author: null,
-  //     title:
-  //       "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-  //     description:
-  //       "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-  //     url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-  //     urlToImage:
-  //       "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-  //     publishedAt: "2020-03-30T15:26:05Z",
-  //     content:
-  //       "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-  //   },
-  //   {
-  //     source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-  //     author: null,
-  //     title:
-  //       "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-  //     description:
-  //       "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-  //     url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-  //     urlToImage:
-  //       "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-  //     publishedAt: "2020-04-27T11:41:47Z",
-  //     content:
-  //       "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-  //   },
-  // ];
-
   constructor() {
     super();
-    console.log("I am a construcotr from News component");
+    console.log("I am a constructor from News component");
     this.state = {
       //articles: this.articles,
       articles:[],
       loading: false,
+      page:1
+
     };
   }
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1ab7b5da78d9442ab0abe5ebdddaebc3";
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1ab7b5da78d9442ab0abe5ebdddaebc3&page=1&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles });
+    this.setState({ articles: parsedData.articles, totalResults:parsedData.totalResults });
+  }
+   handlePreviousClick=async()=>
+  {
+    console.log("previous")
+    let url =
+    `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1ab7b5da78d9442ab0abe5ebdddaebc3&page=${this.state.page-1}&pageSize=20`;
+  let data = await fetch(url);
+  let parsedData = await data.json();
+this.setState(
+{
+  page: this.state.page-1,
+  articles:parsedData.articles
+
+})  
+  }
+   handleNextClick=async()=>
+  {
+    console.log("next")
+    if(this.state.page+1>Math.ceil(this.state.totalResults/20))
+    {
+//no more page available
+    }
+    else{
+    let url =
+    `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1ab7b5da78d9442ab0abe5ebdddaebc3&page=${this.state.page+1}&pageSize=20`;
+  let data = await fetch(url);
+  let parsedData = await data.json();
+  
+this.setState({
+  page: this.state.page+1,
+  articles:parsedData.articles
+})}
   }
   render() {
     return (
-      // <div>
-      //   This is a news component
-      //   <Newsitem />
-      //   <Newsitem />
-      //   <Newsitem />
-      // </div>
-
       <div className="container my-3">
          <Navbar /> 
         <h2>News app- Top Headlines</h2>
@@ -94,6 +76,10 @@ export class News extends Component {
               </div>
             )
           })}
+        </div>
+        <div className="conatiner d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr;Previous</button>
+        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     );
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -13,7 +13,7 @@ export class Newsitem extends Component {
             <div className="card-body">
               <h5 className="card-title">{title}...</h5>
               <p className="card-text">{description}....</p>
-              <a href={newsurl} target="_blank" className="btn btn-primary btn-sm">
+              <a rel="noreferrer" href={newsurl} target="_blank" className="btn btn-dark btn-sm">
                 Read more
               </a>
             </div>
